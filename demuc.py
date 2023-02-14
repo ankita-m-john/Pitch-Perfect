05f5296fae36911d0dc28a936f62afff89825522
ankita-m-john <ankimajohn@gmail.com>
2023-02-14 10:49:21 +0530

    all filesave

---
+++ demuc.py
@@ -0,0 +1,5 @@
+def main():
+    import os
+    os.system('cmd /k "demucs audio/Haha.wav"')
+if __name__=="__main__":
+    main()
---
+++ pitch.py
@@ -0,0 +1,5 @@
+def main():
+    import os
+    os.system('cmd /k "crepe audio/Haha.wav --save-plot"')
+if __name__=="__main__":
+    main()
