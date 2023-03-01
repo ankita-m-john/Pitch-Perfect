f101565baa23d19534195df588b5a6f1be8d7b09
ankita-m-john <ankimajohn@gmail.com>
2023-03-01 21:37:45 +0530

    tried batch again

--- batchpitch.py
+++ batchpitch.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 # specifying the path to csv files
-path = "D:\\Main Project\\Data\\genres_original\\blues\\csv"
+path = "D:\\Main Project\\Dataset\\Data\\genres_original\\classical\\csv"
   
 # csv files in the path
 files = glob.glob(path + "/*.csv")
@@ -18,16 +18,17 @@ content = []
 plt.rcParams["figure.figsize"] = [18.00, 18.00]
 plt.rcParams["figure.autolayout"] = True
 columns = ["time", "frequency"]
-
+#directory = "D:\\Main Project\\Dataset\\Data\\genres_original\\classical\\plots\\"
 # checking all the csv files in the 
 # specified path
 for filename in files:
-    
+    #filepath = directory + filename + '.png'
     # reading content of csv file
     # content.append(filename)
     df = pd.read_csv(filename, usecols=columns)
     plt.plot(df.time, df.frequency)
-    plt.savefig("D:\\Main Project\\Data\\genres_original\\blues\\plots\\"+filename+".png")
+    filename = filename[:-13] + '_' + filename[-12:-7]
+    plt.savefig(filename)
 #    content.append(df)
 # print("Contents in csv file:", df)
 # converting content to data frame
