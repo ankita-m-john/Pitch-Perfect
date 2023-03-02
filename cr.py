3ab61a78756130b9ae61e6a5176d13614defc9bd
ankita-m-john <ankimajohn@gmail.com>
2023-03-02 10:35:46 +0530

    crepe file name

--- cr.py
+++ cr.py
@@ -4,8 +4,10 @@ import os
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+filename = input("Enter file name: ")
+path = "audio\\" + filename
 try:
-    sr, audio = wavfile.read("audio/adio.wav")
+    sr, audio = wavfile.read(path)
 except ValueError:
     print("CREPE: Could not read %s" % file, file=sys.stderr)
     raise
@@ -19,15 +21,17 @@ except ValueError:
     # return path
 time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
 #f0_file = output_path("adio", ".f0.csv","csv\/")
+path= "csv\\" + filename[:-4] + ".csv"
 f0_data = np.vstack([time, frequency]).transpose()
-np.savetxt("csv/adio.fo.csv", f0_data, fmt=['%.3f', '%.3f'], delimiter=',',
+np.savetxt(path, f0_data, fmt=['%.3f', '%.3f'], delimiter=',',
 header='time,frequency', comments='')
 
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 columns = ["time", "frequency"]
-df = pd.read_csv("csv/adio.fo.csv", usecols=columns)
+df = pd.read_csv(path, usecols=columns)
 # print("Contents in csv file:", df)
 plt.plot(df.time, df.frequency)
 # plt.show()
-plt.savefig('plots/adio-f0.png')
+path = "plots\\" + filename[:-4] + ".png"
+plt.savefig(path)
