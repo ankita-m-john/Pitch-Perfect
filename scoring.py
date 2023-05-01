3a3929c9d881d1caec2be6848f5b399d74aa0f87
ankita-m-john <ankimajohn@gmail.com>
2023-05-01 22:05:35 +0530

    completed total scoring

--- scoring.py
+++ scoring.py
@@ -8,8 +8,14 @@ with open("temp_vr.txt", "r+") as file1:
     high_limit = float(file1.readline())
 
 df = pd.read_csv("csv\\Shape Of You.csv")
-print(max(df.frequency),min(df.frequency))
-D = {1: 1000, 2:1100, 3:}
-for i in range(101): D[i] = L[i]
-#((abs(high_limit-max(df.frequency))//max(df.frequency))<=1):
+print(max(df.frequency),min(df.frequency), high_limit, low_limit)
 
+D = {i:1000 + 100*i for i in range(101)}
+x = ((abs(high_limit-max(df.frequency))/max(df.frequency))*100)
+# if x in D:
+#     print(x,D[x])
+y = ((abs(low_limit-min(df.frequency))/min(df.frequency))*100)
+# print(x,1000+100*x)
+# print(y,1000+100*y)
+Total_score = 1000+100*x+100*y
+print(round(Total_score))
