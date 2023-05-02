86582d0a5d90d9f5b49f7c3a387d4a8baeed2eb8
ankita-m-john <ankimajohn@gmail.com>
2023-05-02 22:29:31 +0530

    Working lyric alignment scoring

---
+++ lyricalign/alignmentmatch.py
@@ -0,0 +1,16 @@
+file1 = open("D:\\Main Project\\Pitch-Perfect\\lyricalign\\output.txt",'r')
+file2 = open("D:\\Main Project\\Pitch-Perfect\\lyricalign\\output.txt",'r')
+
+file1_lines = file1.readlines()
+file2_lines = file2.readlines()
+count = 0
+
+for i in range(len(file1_lines)):
+    L1 = file1_lines[i].split('\t')
+    L2 = file2_lines[i].split('\t')
+    if(L1[0] == L2[0]):
+        count = count + 1
+print("Match percentage: {:.2f}".format((count/(i+1))*100))
+
+file1.close()
+file2.close()
