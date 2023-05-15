6c40f2c31225ab0c407f8a470b7f282164983ef7
ankita-m-john <ankimajohn@gmail.com>
2023-05-15 12:25:48 +0530

    req

--- sql.py
+++ sql.py
@@ -4,22 +4,22 @@ import mysql.connector
 myconn = mysql.connector.connect(host = "127.0.0.1", user = "root",password = "ankita", auth_plugin='mysql_native_password', database = "Pitch_Perfect")  
   
 #printing the connection object   
-print(myconn)  
+# print(myconn)  
 #creating the cursor object  
 cur = myconn.cursor()  
 
 try:  
     #     #creating a new database  
     # cur.execute("create database Pitch_Perfect")  
-    # dbs = cur.execute("show databases")  
-    # dbs = cur.execute("create table Song(name varchar(20) not null, artist int(20) not null, song_id int(20) autoincrement primary key,genre varchar(50), mp3 varchar(100) not null, csv varchar(100) not null, Total_Score int(20))") 
-    # dbs = cur.execute("create table User(name varchar(20) not null, id int(20) not null primary key, low_note float not null, high_note float not null)") 
-    # dbs = cur.execute("create table Scoring(user_id varchar(20) not null, song_id int(20) not null primary key, total_score )") 
+    dbs = cur.execute("desc table Song") 
+    # dbs = cur.execute("create table Song(song_id int(20) autoincrement primary key, name varchar(20) not null, artist int(20) not null, genre varchar(50), mp3 varchar(100) not null, csv varchar(100) not null, lyrics varchar(100) not null)") 
+    # dbs = cur.execute("create table User(user_id int(20) not null primary key, name varchar(20) not null, low_note float not null, high_note float not null)") 
+    # dbs = cur.execute("create table Scoring(user_id varchar(20) not null, song_id int(20) not null primary key, Total_Score int(20))") 
     # dbs = cur.execute("alter table Song add artist")
     # dbs = cur.execute("insert into Song(name,id,mp3,csv) values("Shape Of You","")")
-    dbs = cur.execute("insert into Song(name,id,mp3,csv) values("Shape Of You","")")
+    # dbs = cur.execute("drop table User")  
 except:  
     myconn.rollback()  
 for x in cur:  
-    print(x)  
+     print(x)  
 myconn.close()  
