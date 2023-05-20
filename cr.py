29479a00677b5090c11e975b1e771258a67905ef
ankita-m-john <ankimajohn@gmail.com>
2023-05-20 11:41:16 +0530

    Database files all completed except background music

--- cr.py
+++ cr.py
@@ -6,15 +6,15 @@ import numpy as np
 # from matplotlib import pyplot as plt
 import sys
 def pitch_detect():
-    path = "D:\\Main Project\\Pitch-Perfect\\separated\\htdemucs\\The Gambler\\vocals.wav"
+    path = "D:\\Main Project\\Pitch-Perfect\\separated\\htdemucs\\song\\vocals.wav"
     try:
         sr, audio = wavfile.read(path)
     except ValueError:
-        print("CREPE: Could not read The Gambler", file=sys.stderr)
+        print("CREPE: Could not read song", file=sys.stderr)
         raise
     time, frequency, confidence, activation = crepe.predict(audio, sr, viterbi=True)
     #f0_file = output_path("adio", ".f0.csv","csv\/")
-    path= "csv\\The Gambler.csv"
+    path= "csv\\song.csv"
     f0_data = np.vstack([time, frequency]).transpose()
     np.savetxt(path, f0_data, fmt=['%.3f', '%.3f'], delimiter=',',
     header='time,frequency', comments='')
